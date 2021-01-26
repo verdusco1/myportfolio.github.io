16fadbb563805a5574288a84c3cea0aa47fea1cf
Eduardo Verdusco <em.verdusco@gmail.com>
2021-01-25 17:25:42 -0800

    updated files
    updated files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Projects from './Projects.js';
+import Articles from './Articles.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
 
+    // Router set up
+    <Route exact path="/" component={Projects} />
+    <Route path="/articles" component={Articles} />
+    <Route path="/about" component={About} />
+
+    <div className="App">
+      <nav id="nav-container">
+        <ul class="nav-links">
+          <li><Link to="/" className="item">Projects</Link></li>
+          <li><Link to="/articles" className="item">Articles</Link></li>
+          <li><Link to="/about" className="item">About</Link></li>
+        </ul>
+      </nav>
     </div>
+    </BrowserRouter>
   );
 }
 
