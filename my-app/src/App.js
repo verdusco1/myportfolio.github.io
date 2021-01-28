8539fd7280fc04641a601c42ffcdbd3e127d7e41
Eduardo Verdusco <em.verdusco@gmail.com>
2021-01-28 09:39:08 -0800

    updated node package

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Projects from './Projects.js';
-import Articles from './Articles.js';
-import About from './About.js';
+import Projects from './components/Projects.js';
+import Articles from './components/Articles.js';
+import About from './components/About.js';
 import './App.css';
 
 function App() {
@@ -12,19 +12,21 @@ function App() {
 
     // Router set up
     <Route exact path="/" component={Projects} />
-    <Route path="/articles" component={Articles} />
-    <Route path="/about" component={About} />
+    <Route path="/components/articles" component={Articles} />
+    <Route path="/components/about" component={About} />
 
     <div className="App">
       <nav id="nav-container">
         <ul class="nav-links">
           <li><Link to="/" className="item">Projects</Link></li>
-          <li><Link to="/articles" className="item">Articles</Link></li>
-          <li><Link to="/about" className="item">About</Link></li>
+          <li><Link to="/components/articles" className="item">Articles</Link></li>
+          <li><Link to="/components/about" className="item">About</Link></li>
         </ul>
       </nav>
 
     <div>
+      <Projects />
+      <Articles />
     <About />
     </div>
 
