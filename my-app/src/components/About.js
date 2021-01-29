7b337aad026fc61c1073e63ae802cb1f9dfebc54
Eduardo Verdusco <em.verdusco@gmail.com>
2021-01-28 16:09:49 -0800

    Updated about.js
    Created general layout for about.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,21 +1,27 @@
 import React from "react";
+import background from "../images/background.jpg";
 import '../App.css';
 
 
 
 function About(props) {
     return (
-        <div>
-        <div class="about-container">
-            </div>
 
-        <div class="about-text">
-            <h1>Hello, my name is Eddie</h1>
-        <button type="button" class="btn btn-primary">
-            bootstrap
-    </button>
-           </div>
+        <div class="">
+
+        <div class="about-background">
+        <h1 class="hello-text">Hello, I'm Eddie</h1>
+        <h2  class="hello-text">An aspiring fron-end developer that loves to create.</h2>
+        <img class="hello-image" src={background} width='50' ></img>
         </div>
+
+        <div class="row p-0">
+            <h2 class="about-text p-0">I am an aspiring fron-end developer that loves to create.</h2>
+           </div>
+
+
+       </div>
+
     )
 }
 
