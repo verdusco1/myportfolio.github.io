a32717eccb03ac307e337ea6645ed866249fc236
Eduardo Verdusco <em.verdusco@gmail.com>
2021-01-29 12:22:03 -0800

    updated navbar.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,5 +1,7 @@
 import React from "react";
 import background from "../images/background.jpg";
+import avatar from "../images/avataaars.png";
+import github from '../images/Github_5.png';
 import '../App.css';
 
 
@@ -8,26 +10,21 @@ function About(props) {
     return (
 
         <div class="container-fluid" >
-            <div class="wrapper">
-                <img src={background} class="img-responsive" alt="Responsive image" />
-            </div>
-            <div class="img-title">
+
+            <div class="about-background"></div>
+
+
+
+
+            <div class="home-text">
                 <h1>Hi, I'm Eddie</h1>
                 <h3>An aspiring front-end developer that loves to create things.</h3>
-                <Avatar
-  avatarStyle='Circle'
-  topType='ShortHairTheCaesar'
-  accessoriesType='Prescription02'
-  hairColor='Black'
-  facialHairType='BeardLight'
-  facialHairColor='Black'
-  clotheType='BlazerShirt'
-  eyeType='Default'
-  eyebrowType='DefaultNatural'
-  mouthType='Smile'
-  skinColor='Light'
-/>
             </div>
+
+            <img class="avatar" src={avatar} alt="Eddie's avatar"/>
+
+            <div class="col-lg-12 banner-background">When I look back to the first application that I created, I remember being so excited when everything finally rendered. A spark was ignited that day and the path to self-taught programming was revealed. As time has gone my passion for development has only grown and is fueled by my excitement of always being able to learn something new.</div>
+
        </div>
 
     )
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,19 +4,31 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Projects from './Projects.js';
 import Contact from './Contact.js';
 import About from './About.js';
+import github from '../images/Github_5.png';
+import linkedin from '../images/Linkedin_5.png';
+import avatar from "../images/avataaars.png";
 
 function Navbar(props) {
     return (
 <div class="navbar-container">
-      <nav class="navbar navbar-default">
+<nav class="navbar navbar-default">
         <ul class="nav navbar-nav">
-          <li><a href="">About</a></li>
+          <li ><a href="">About</a></li>
           <li><a href="">Projects</a></li>
           <li><a href="">Contact</a></li>
         </ul>
-      </nav>
-</div>
 
+
+    <div class="social-links" >
+<img class="" src={github} alt="Eddie's avatar"/>
+<img class="" src={linkedin} alt="Eddie's avatar"/>
+</div>
+      </nav>
+
+
+
+
+</div>
 
 
 
