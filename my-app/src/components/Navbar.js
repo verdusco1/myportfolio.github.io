18b83fbaa98e58cb6ed69c46e4e87763d97ed9b6
Eduardo Verdusco <em.verdusco@gmail.com>
2021-01-29 14:45:23 -0800

    testing

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -10,8 +10,7 @@ import avatar from "../images/avataaars.png";
 
 function Navbar(props) {
     return (
-<div class="navbar-container">
-
+<div class="">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
