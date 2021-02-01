322cddcbd77295404f47837cbba1d026e18ec68e
Eduardo Verdusco <em.verdusco@gmail.com>
2021-02-01 14:53:47 -0800

    updated tile layout for projects.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -8,28 +8,17 @@ import '../App.css';
 function About(props) {
     return (
 
-        <div class="container-fluid" >
-<div class="about-background"></div>
-
-
-
-
-
-<div class="home-text">
-    <h1>Hi, I'm Eddie</h1>
-    <h3>An aspiring front-end developer that loves to create things.</h3>
-</div>
-
-<img class="avatar" src={avatar} alt="Eddie's avatar"/>
-
-<div class="col-lg-12 banner-background">
-    <p class="col-sm-5 banner-text">
+    <div class="container-fluid" >
+        <div class="home-text">
+            <h1>Hi, I'm Eddie</h1>
+            <h3>An aspiring front-end developer that loves to create things.</h3>
+        </div>
+        <div class="row bio-container">
+            <img class="avatar" src={avatar} alt="Eddie's avatar"/>
+            <p class="col-sm-6">
     When I look back to the first application that I created, I remember being so excited when everything finally rendered. A spark was ignited that day and the path to self-taught programming was revealed. As time has gone my passion for development has only grown and is fueled by my excitement of always being able to learn something new.
     </p>
-            </div>
-
-
-
+        </div>
 
        </div>
 
--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -3,8 +3,20 @@ import background from "../images/background.jpg";
 
 function Projects(props) {
     return (
-        <div class="projects-container">
-            <h1>Projects</h1>
+        <div class="container-fluid">
+            <h1>Latest Work</h1>
+
+            <div class=" project-tile ">
+            <div class="project-desc">
+                <h3>Project Title</h3>
+                </div>
+                <img class=" "src={background}/>
+                <div class="project-buttons  ">
+                <button type="button" class="btn btn-primary"><a hfer="#">Code</a></button>
+                <button type="button" class="btn btn-primary"><a hfer="#">Live</a></button>
+                </div>
+
+            </div>
 
 
 
