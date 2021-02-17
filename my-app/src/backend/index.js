ee41d9251d6ee1a01a06bda981a9f9b84f4e06c3
Eduardo Verdusco <em.verdusco@gmail.com>
2021-02-17 12:03:35 -0800

    Update index.js

--- my-app/src/backend/index.js
+++ my-app/src/backend/index.js
@@ -1,13 +1,4 @@
-const express = require("express");
-const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 
 router.post("/contact", (req, res) => {
     const name = req.body.name;
